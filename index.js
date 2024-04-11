18f773a570aa7628dac1df269ea8bd72dda9319a
saikrishnasirra45 <saikrishnasirra@gmail.com>
2024-04-11 11:07:09 +0530

    feature signup working

---
+++ index.js
@@ -0,0 +1,18 @@
+let h1Element = document.createElement("h1");
+h1Element.textContent = "Web Technologies";
+h1Element.style.color = "white";
+let containerElement = document.getElementById("myContainer");
+containerElement.appendChild(h1Element);
+let btnElement = document.createElement("button");
+btnElement.textContent = "Change Heading and Add Styles";
+containerElement.appendChild(btnElement);
+btnElement.onclick = function(){
+    h1Element.textContent = "4.0 Technologies";
+    h1Element.classList.add("heading");
+  };
+let removeStylesBtnElement = document.createElement("button");
+removeStylesBtnElement.textContent = "Remove Styles";
+containerElement.appendChild(removeStylesBtnElement);
+removeStylesBtnElement.onclick = function(){
+    h1Element.classList.remove("heading");
+  };
